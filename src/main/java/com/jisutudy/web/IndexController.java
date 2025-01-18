81efb0fafc61e70c81a9e66d82c1c4487d986752
Jisu-Shin <sjszzang3053@gmail.com>
2025-01-18 21:39:02 +0900

    feat : index, sms-send 머스테치 레이아웃 세팅

--- src/main/java/com/jisutudy/web/IndexController.java
+++ src/main/java/com/jisutudy/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/sms/send")
+    public String smsSend() {
+        return "sms-send";
+    }
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,11 +1,35 @@
 var main = {
     init : function () {
+        var _this = this;
+        $('#btn-add').on('click', function () {
+            _this.add();
+        });
+    },
+
+    add : function () {
+        var phonenumber = $('#txt-phonenumber').val();
+        if (main.isEmpty(phonenumber)) {
+            alert("전화번호를 입력해주세요.")
+
+        } else {
+            var insertTr = "";
+            insertTr += "<tr><td>";
+            insertTr += phonenumber;
+            insertTr += "</td></tr>";
+            $('tbody').append(insertTr);
+            $('#txt-phonenumber').val("");
+        }
+
+    },
+
+    isEmpty : function (value) {
+        if(value == "" || value == null || value == undefined) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    save : function () {
-    }
-
-
-
 
 };
+
+main.init();
