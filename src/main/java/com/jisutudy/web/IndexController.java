f49f48b4e258ad21b7b34e9cb34bb98d193e7dca
Jisu-Shin <sjszzang3053@gmail.com>
2025-01-30 02:12:50 +0900

    프론트단 변경 - sms 발송 - 고객 전체 조회

--- src/main/java/com/jisutudy/web/IndexController.java
+++ src/main/java/com/jisutudy/web/IndexController.java
@@ -1,10 +1,16 @@
 package com.jisutudy.web;
 
+import com.jisutudy.service.JpaCustService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final JpaCustService custService;
 
     @GetMapping
     public String index(){
@@ -16,8 +22,19 @@ public class IndexController {
         return "sms-send";
     }
 
+    @GetMapping("/sms/sendList")
+    public String findAllSmsList() {
+        return "sms-sendlist";
+    }
+
     @GetMapping("/cust/save")
     public String saveCust() {
         return "cust-save";
+    }
+
+    @GetMapping("/cust/findAll")
+    public String findAllCust(Model model) {
+        model.addAttribute("custs",custService.findAll());
+        return "cust-findAll";
     }
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -22,8 +22,16 @@ var main = {
         $('#btn-choiceCust').on('click', function () {
             $('#div-addPhoneNumber').hide();
             $('#div-choiceCust').show();
-            console.log("고객 조회 시작");
-            oper.ajax('GET','','/custs',main.choiceCustCallback);
+//            console.log("고객 전체 조회 시작");
+            oper.ajax('GET','','/api/custs',callback.choiceCust);
+        });
+
+        $('#btn-send').on('click', function () {
+            console.log("체크된 고객 id");
+            var checkbox = $("input[name=chb_cust]:checked");
+            console.log(checkbox);
+
+            _this.sendSms();
         });
 
     },
@@ -50,29 +58,55 @@ var main = {
             phoneNumber: $('#phonenumber').val(),
             smsConsentType: $('#smsConsentType').val()
         }
-        oper.ajax("POST",data,'/api/v1/cust', main.saveCustCallback);
+        oper.ajax("POST",data,'/api/custs', callback.saveCust);
     },
 
-    saveCustCallback : function () {
+    sendSms : function () {
+        var checkbox = $("input[name=chb_cust]:checked");
+//        checkbox.each({})
+        var tr = checkbox.parent().parent();
+        var td = tr.children();
+//        console.log(td.eq(1).text());
+//        console.log(td.eq(2).text());
+        console.log(oper.getTodayDt());
+
+        var data = {
+                custId: td.eq(1).text(),
+                smsContent: $('#content').val(),
+                sendDt : oper.getTodayDt() ,
+                smsType : "01"
+            }
+        oper.ajax("POST",data,'/api/sms/send', callback.sendSms);
+    }
+
+};
+
+var callback = {
+    saveCust : function () {
         alert("고객 등록이 완료되었습니다");
         window.location.href='/cust/save';
     } ,
 
-    choiceCustCallback : function (data) {
+    choiceCust : function (data) {
         console.log("고객 조회 완료");
         var tbody = $('#tbody');
         tbody.empty();
         data.forEach(cust => {
             var row = '<tr>'
-            row+='<td><input type="checkbox" value="" </td>'
+            row+='<td><input type="checkbox" name="chb_cust" value="" </td>'
+            row+='<td>'+cust.id+'</td>'
             row+='<td>'+cust.name+'</td>'
-            row+='<td>'+cust.phoneNumber+'</td>'
             row+='</tr>'
             tbody.append(row);
         });
+    } ,
+
+    sendSms : function () {
+        alert("sms 발송이 완료되었습니다");
+        window.location.href='/sms/send';
     }
 
-};
+}
 
 var oper = {
     isEmpty : function (value) {
@@ -93,6 +127,16 @@ var oper = {
         }).done(function (response){
             callback(response);
         });
+    },
+
+    getTodayDt : function() {
+        let today = new Date();
+        let year = String(today.getFullYear());
+        let month = String(today.getMonth()+1).padStart(2,"0");
+        let date = String(today.getDate()).padStart(2,"0");
+        let hours = String(today.getHours()).padStart(2,"0");
+        let minutes = String(today.getMinutes()).padStart(2,"0");
+        return year+month+date+hours+minutes;
     }
 }
 
