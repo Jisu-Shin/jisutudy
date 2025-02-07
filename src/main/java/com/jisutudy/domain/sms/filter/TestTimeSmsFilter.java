376420bbed29c79d7ffcbf4de78ef797f89651b4
Jisu-Shin <sjszzang3053@gmail.com>
2025-02-07 23:06:56 +0900

    update

--- src/main/java/com/jisutudy/domain/sms/filter/TestTimeSmsFilter.java
+++ src/main/java/com/jisutudy/domain/sms/filter/TestTimeSmsFilter.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-@Profile("test")
+@Profile("dev")
 @Component
 public class TestTimeSmsFilter implements TimeSmsFilter{
     @Override
