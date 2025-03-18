7dcd0893802a0a24a78d4fbcfbb292b9614ebebc
Jisu-Shin <sjszzang3053@gmail.com>
2025-03-19 00:35:29 +0900

    rename test smsservice

--- src/main/java/com/jisutudy/domain/sms/springstudy/TestSmsService.java
+++ src/main/java/com/jisutudy/domain/sms/springstudy/TestSmsService.java
@@ -1,12 +1,11 @@
 package com.jisutudy.domain.sms.springstudy;
 
-import com.jisutudy.domain.sms.Sms;
 import com.jisutudy.domain.sms.SmsResult;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface SmsService {
+public interface TestSmsService {
     SmsResult sendSms(Sms sms);
     List<Sms> findSmsList(LocalDateTime startDt, LocalDateTime endDt);
 }
--- src/main/java/com/jisutudy/domain/sms/springstudy/TestSmsServiceImpl.java
+++ src/main/java/com/jisutudy/domain/sms/springstudy/TestSmsServiceImpl.java
@@ -2,18 +2,15 @@ package com.jisutudy.domain.sms.springstudy;
 
 import com.jisutudy.domain.customer.Cust;
 import com.jisutudy.domain.customer.springstudy.CustRepository;
-import com.jisutudy.domain.sms.Sms;
 import com.jisutudy.domain.sms.SmsResult;
 import com.jisutudy.domain.sms.filter.SmsFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Component
 @RequiredArgsConstructor
-public class SmsServiceImpl implements SmsService{
+public class TestSmsServiceImpl implements TestSmsService {
     private final SmsRepository smsRepository;
     private final CustRepository custRepository;
     private final SmsFilter smsFilter;
@@ -22,12 +19,12 @@ public class SmsServiceImpl implements SmsService{
     public SmsResult sendSms(Sms sms) {
         // 고객 전화번호 세팅
         Cust cust = custRepository.findById(sms.getCustId());
-        sms.setCustPhoneNumber(cust.getPhoneNumber());
+//        sms.setCustPhoneNumber(cust.getPhoneNumber());
 
         // 필터링
-        SmsResult smsResult = smsFilter.filter(sms);
+//        SmsResult smsResult = smsFilter.filter(sms);
+        SmsResult smsResult = SmsResult.SUCCESS;
 
-        // TODO sms 상태? 처리결과를 저장하는 필드가 필요하다
         smsRepository.save(sms);
         return smsResult;
     }
