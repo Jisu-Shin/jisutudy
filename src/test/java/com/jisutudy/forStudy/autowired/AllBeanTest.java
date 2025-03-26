01c1297f5bf8daf88d0dd6b03781137eabe0e879
Jisu-Shin <sjszzang3053@gmail.com>
2025-03-27 02:00:23 +0900

    Test : 테스트 코드 수정

--- src/test/java/com/jisutudy/forStudy/autowired/AllBeanTest.java
+++ src/test/java/com/jisutudy/forStudy/autowired/AllBeanTest.java
@@ -1,10 +1,15 @@
 package com.jisutudy.forStudy.autowired;
 
 import com.jisutudy.AppConfig;
-import com.jisutudy.domain.sms.filter.TimeSmsFilter;
+import com.jisutudy.service.filter.TimeSmsFilter;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Import;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,24 +19,30 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+//@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=prod")
+@Import(AllBeanTest.FilterService.class)
 public class AllBeanTest {
+
+    @Autowired
+    ApplicationContext ac;
 
     @Test
     void findAllBean() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class, FilterService.class);
+//        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class, FilterService.class);
         FilterService filterService = ac.getBean(FilterService.class);
         LocalDateTime ldt = LocalDateTime.of(LocalDate.now(), LocalTime.of(21,00));
-        boolean testResult = filterService.filter(ldt, "testTimeSmsFilter");
         boolean prodResult = filterService.filter(ldt, "prodTimeSmsFilter");
-
-        System.out.println("testResult = " + testResult);
         System.out.println("prodResult = " + prodResult);
-
-        assertThat(testResult).isTrue();
         assertThat(prodResult).isFalse();
+
+//        boolean testResult = filterService.filter(ldt, "testTimeSmsFilter");
+//        System.out.println("testResult = " + testResult);
+//        assertThat(testResult).isTrue();
 
     }
 
+    @TestConfiguration
     @RequiredArgsConstructor
     static class FilterService {
         private final Map<String, TimeSmsFilter> filterMap;
@@ -46,5 +57,6 @@ public class AllBeanTest {
             return timeSmsFilter.isSendable(ldt);
         }
     }
+
 
 }
