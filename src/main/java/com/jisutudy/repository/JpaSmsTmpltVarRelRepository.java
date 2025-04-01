70296b70b0debc0ed40ee7f3beff693662547777
Jisu-Shin <sjszzang3053@gmail.com>
2025-04-02 01:33:51 +0900

    Remove QueryDSL generated Q classes from Git tracking

--- src/main/java/com/jisutudy/repository/JpaSmsTmpltVarRelRepository.java
+++ src/main/java/com/jisutudy/repository/JpaSmsTmpltVarRelRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface JpaSmsTemplateVarRelRepository extends JpaRepository<SmsTmpltVarRel, SmsTmpltVarRelId> {
+public interface JpaSmsTmpltVarRelRepository extends JpaRepository<SmsTmpltVarRel, SmsTmpltVarRelId> {
     List<SmsTmpltVarRel> findBySmsTmpltVarRelId_SmsTmpltId(Long smsTmpltId);
 }
