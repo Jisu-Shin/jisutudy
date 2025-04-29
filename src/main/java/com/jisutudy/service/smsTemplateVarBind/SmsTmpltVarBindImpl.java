ad2bbda34a5bfd7d970cbb0f82fc7836f6c90bdb
Jisu-Shin <sjszzang3053@gmail.com>
2025-04-30 00:54:53 +0900

    chore : 로그추가

--- src/main/java/com/jisutudy/service/smsTemplateVarBind/SmsTmpltVarBindImpl.java
+++ src/main/java/com/jisutudy/service/smsTemplateVarBind/SmsTmpltVarBindImpl.java
@@ -4,6 +4,7 @@ import com.jisutudy.domain.SmsTemplate;
 import com.jisutudy.domain.TemplateVariable;
 import com.jisutudy.domain.TemplateVariableType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SmsTmpltVarBindImpl implements SmsTmpltVarBinder {
@@ -32,10 +34,14 @@ public class SmsTmpltVarBindImpl implements SmsTmpltVarBinder {
                         Collectors.mapping(rel -> rel.getTemplateVariable(), Collectors.toList())
                 ));
 
+        log.info("@@@@@ 템플릿 변수 유형별로 그룹핑 완료");
+
         for (Map.Entry<TemplateVariableType, List<TemplateVariable>> entry : grouping.entrySet()) {
             if (entry.getValue().size() > 0) {
                 VariableBinder variableBinding = variableBinderMap.get(entry.getKey().getClassName());
+                log.info("@@@@@ 바인드임플에서 값 가져오기 전");
                 variableBinding.getValues(entry.getValue(), bindingDto).forEach((k, v) -> replacements.put(k, v));
+                log.info("@@@@@ 바인드임플에서 값 가져온 후 ");
             }
         }
 
